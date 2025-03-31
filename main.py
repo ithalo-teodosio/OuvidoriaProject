20fe6c9c1ea783860ca8cef51631bbda4252b087
Ithalo Teodósio <ithalodust@hotmail.com>
2025-03-31 17:13:22 -0300

    Atualizar o main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from ouvidoria_def import *
 
 endSGBD = 'localhost'
 userSGBD = 'root'
-senhaSGBD = 'himegami110395'
+senhaSGBD = 'SUA_SENHA'
 schemaSGBD = 'ouvidoria_xyz'
 
 connection = criarConexao(endSGBD, userSGBD, senhaSGBD, schemaSGBD)
